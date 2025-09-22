fe511e968ba583ca8738a6de0fe6081a504af736
Guilherme Moreira <devguilhermemoreira@gmail.com>
2025-09-22 18:14:14 -0300

    criação das models categories, expenses e incomes

---
+++ models/categories_model.py
@@ -0,0 +1,20 @@
+from sqlalchemy import (
+    Column,
+    BigInteger,
+    String,
+    Boolean,
+    DateTime,
+    func
+)
+from utils.database_util import Base
+
+class Categories(Base):
+    __tablename__ = "categories"
+    __table_args__ = {"schema": "public"}
+
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    is_expense = Column(Boolean, nullable=False, default=True)
+    is_active = Column(Boolean, nullable=False, default=True)
+    created_at = Column(DateTime, nullable=False, server_default=func.now())
+    updated_at = Column(DateTime, nullable=True, onupdate=func.now())
---
+++ models/incomes_model.py
@@ -0,0 +1,27 @@
+from sqlalchemy import (
+    Column,
+    BigInteger,
+    SmallInteger,
+    String,
+    Numeric,
+    Boolean,
+    DateTime,
+    ForeignKey,
+    func
+)
+from utils.database_util import Base
+
+class Incomes(Base):
+    __tablename__ = "incomes"
+    __table_args__ = {"schema": "public"}
+
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
+    user_id = Column(BigInteger, ForeignKey("public.users.id", ondelete="CASCADE"), nullable=False)
+    category_id = Column(BigInteger, ForeignKey("public.categories.id", ondelete="CASCADE"), nullable=False)
+    amount = Column(Numeric(5, 2), nullable=False)
+    due_day = Column(SmallInteger, nullable=False)
+    description = Column(String, nullable=False)
+    description_detail = Column(String, nullable=True)
+    is_active = Column(Boolean, nullable=False, default=True)
+    created_at = Column(DateTime, nullable=False, server_default=func.now())
+    updated_at = Column(DateTime, nullable=True, onupdate=func.now())
