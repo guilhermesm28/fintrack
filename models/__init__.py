8d22c4de756666df18313db3b603eaf821616168
Guilherme Moreira <devguilhermemoreira@gmail.com>
2025-09-27 18:43:33 -0300

    ajuste na FK expenses e inclusão da model no __init__

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 from .categories import Categories
 from .expenses import Expenses
 from .incomes import Incomes
+from .expense_allocations import ExpenseAllocations
 
-__all__ = ["Categories", "Expenses", "Incomes"]
+__all__ = ["Categories", "Expenses", "Incomes", "ExpenseAllocations"]
--- models/expense_allocations.py
+++ models/expense_allocations.py
@@ -17,7 +17,7 @@ class ExpenseAllocations(Base):
     id = Column(BigInteger, primary_key=True, autoincrement=True)
     user_id = Column(BigInteger, ForeignKey("public.users.id", ondelete="CASCADE"), nullable=False)
     income_id = Column(BigInteger, ForeignKey("public.incomes.id", ondelete="CASCADE"), nullable=False)
-    expense_id = Column(BigInteger, ForeignKey("public.expense.id", ondelete="CASCADE"), nullable=False)
+    expense_id = Column(BigInteger, ForeignKey("public.expenses.id", ondelete="CASCADE"), nullable=False)
     allocated_amount = Column(Numeric(5, 2), nullable=False)
     is_active = Column(Boolean, nullable=False, default=True)
     created_at = Column(DateTime, nullable=False, server_default=func.now())
